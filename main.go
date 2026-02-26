7cdda60ffdedafaa0061426ce36f28484fea4419
Joseph Presley <jpresley@synthesis.health>
2026-02-26 14:21:36 -0800

    feat: add main with flag parsing and password output

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	length := flag.Int("length", 20, "password length")
+	special := flag.String("special", "", "allowed special characters (replaces defaults)")
+	noUpper := flag.Bool("no-upper", false, "exclude uppercase letters")
+	noDigits := flag.Bool("no-digits", false, "exclude digits")
+	noSpecial := flag.Bool("no-special", false, "exclude special characters")
+	noAmbiguous := flag.Bool("no-ambiguous", false, "exclude ambiguous characters (0OIl1)")
+
+	flag.Parse()
+
+	opts := Options{
+		Length:      *length,
+		NoUpper:     *noUpper,
+		NoDigits:    *noDigits,
+		NoSpecial:   *noSpecial,
+		Special:     *special,
+		NoAmbiguous: *noAmbiguous,
+	}
+
+	pw, err := Generate(opts)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(pw)
+}
